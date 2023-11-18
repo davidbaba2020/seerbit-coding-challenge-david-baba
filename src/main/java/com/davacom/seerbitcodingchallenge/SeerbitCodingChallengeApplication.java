0b12e9f83f4276f2371b7b2b41965c2d42a456d1
davidbaba <davidbabasalifu2020@gmail.com>
2023-11-19 00:00:10 +0100

    Initial project setup

---
+++ src/main/java/com/davacom/seerbitcodingchallenge/SeerbitCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.davacom.seerbitcodingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeerbitCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeerbitCodingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davacom/seerbitcodingchallenge/SeerbitCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davacom.seerbitcodingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeerbitCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
