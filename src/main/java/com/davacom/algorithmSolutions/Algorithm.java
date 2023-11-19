c76d4a835a457036dc1c264d0acb858f1eb90a3f
davidbaba <davidbabasalifu2020@gmail.com>
2023-11-19 01:21:59 +0100

    modified display on algorithm solution display

--- src/main/java/com/davacom/algorithmSolutions/Algorithm.java
+++ src/main/java/com/davacom/algorithmSolutions/Algorithm.java
@@ -15,10 +15,10 @@ public class Algorithm {
         int target1 = 9;
         int target2 = 26;
         int target3 = 10;
-        System.out.println("The array supplied for all target is: "+ Arrays.toString(array));
-        System.out.println("For target1 as 9:"+hasTwoSum(array, target1));
-        System.out.println("For target2 as 26:"+hasTwoSum(array, target2));
-        System.out.println("For target3 as 10:"+hasTwoSum(array, target3));
+        System.out.println("The array supplied for all target is:  "+ Arrays.toString(array));
+        System.out.println("For target1 as 9:   "+hasTwoSum(array, target1));
+        System.out.println("For target2 as 26:  "+hasTwoSum(array, target2));
+        System.out.println("For target3 as 10:  "+hasTwoSum(array, target3));
         System.out.println(" ");
 
         System.out.println("*********************Algorithm 2*************************");
@@ -28,10 +28,10 @@ public class Algorithm {
         int target5 = 5;
         int target6 = 7;
 
-        System.out.println("The array supplied for all target is: "+ Arrays.toString(array2));
-        System.out.println("For target4 as 1:"+Arrays.toString(findIndices(array2,target4)));
-        System.out.println("For target5 as 5:"+Arrays.toString(findIndices(array2,target5)));
-        System.out.println("For target6 as 17:"+Arrays.toString(findIndices(array2,target6)));
+        System.out.println("The array supplied for all target is:  "+ Arrays.toString(array2));
+        System.out.println("For target4 as 1:  "+Arrays.toString(findIndices(array2,target4)));
+        System.out.println("For target5 as 5:  "+Arrays.toString(findIndices(array2,target5)));
+        System.out.println("For target6 as 17: "+Arrays.toString(findIndices(array2,target6)));
         System.out.println(" ");
 
         System.out.println("*********************Algorithm 3*************************");
@@ -41,8 +41,8 @@ public class Algorithm {
         intervals.add(new Interval(2, 6));
         intervals.add(new Interval(8, 10));
         intervals.add(new Interval(15, 18));
-        System.out.println("The supplied intervals are : "+ intervals);
-        System.out.println("The merged intervals are : "+mergeIntervals(intervals));
+        System.out.println("The supplied intervals are:  "+ intervals);
+        System.out.println("The merged intervals are :   "+mergeIntervals(intervals));
         System.out.println("**********************************************");
 
 
